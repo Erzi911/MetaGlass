2afd526d65d10338376b2b16e6258f6556c4863b
Yerassyl <marshall2508l@gmail.com>
2026-02-04 11:02:47 +0500

    .

--- contract/NRD.sol
+++ contract/NRD.sol
@@ -6,12 +6,17 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Nerd is ERC20, Ownable{
     uint256 public constant maxS=1000*10**18;
     uint256 public constant maxU=100*10**18;
+    address[] public hs;
 }
+event queue(address indexed user, uint256 amount, string status);
 constructor() ERC20("MetaGlass", "MTG") {}
 function mint(address to, uint256 kol) public onlyOwner {
-    require(totalSupply() + kol <= maxS);
+    require(balanceOf(to) + kol <= maxU);
     _mint(to, kol);
 }
-function status(address account) public view returns (string memory){
-    uint256 wall=balanceOf(account);
-    }
+function status(address acc) public view returns(string memory){
+    uint256 b=balanceOf(acc);
+}
+function restrict(){}
+
+function queue(){}
