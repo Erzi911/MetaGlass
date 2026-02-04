8fc73b365c20e4c669a0cc51723bc6584797f717
Yerassyl <marshall2508l@gmail.com>
2026-02-05 00:49:28 +0500

    .

--- contract/NRD.sol
+++ contract/NRD.sol
@@ -1,16 +1,20 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.31;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+    import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Nerd is ERC20, Ownable{
-    uint256 public constant maxS=1000*10**18;
-    uint256 public constant maxU=100*10**18;
+    uint256 public maxU=100*10**18;
     address[] public hs;
+
 }
 event queue(address indexed user, uint256 amount, string status);
-constructor() ERC20("MetaGlass", "MTG") {}
+constructor() ERC20("MetaGlass", "MTG") {
+    _mint(msg.sender, 100*10**18);
+    hs.push(msg.sender);
+}
 function mint(address to, uint256 kol) public onlyOwner {
+    require(totalSupply() + kol <= 1000*10**18);
     require(balanceOf(to) + kol <= maxU);
     _mint(to, kol);
 }
---
+++ contract/smartcont.sol
@@ -0,0 +1,4 @@
+//SPDX-llicense-Identifier: UNLICENSED
+pragma solidity ^0.8.31;
+import './NRD.sol';
+contract 
