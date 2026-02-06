b7c7bf5918487b1f2bab95716526fdb63d77128a
Yerassyl <marshall2508l@gmail.com>
2026-02-06 15:22:51 +0500

    added fee

--- contract/NRD.sol
+++ contract/NRD.sol
@@ -8,6 +8,7 @@ contract Nerd is ERC20, Ownable{
     address[] public hs;
     mapping(address=>uint256) public position;
     mapping(address=>bool) public inHs;
+    uint16 public feeBps=200;
 
 event Queue(address indexed user, uint256 amount, string status);
 constructor() ERC20("MetaGlass", "MTG") Ownable(msg.sender){
